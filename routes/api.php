f38fc868e89cdae37a9a04499a5600f27513c16c
mohfikryansyah <moh.fikryansyah@gmail.com>
2024-10-23 23:39:50 +0800

    employee controller

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DevisiController;
 use App\Http\Controllers\OfficeController;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\AttendanceController;
-use App\Http\Controllers\DevisiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,7 +30,7 @@ Route::post('/login', [AuthController::class, 'login'])
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/attendances', [AttendanceController::class, 'store']);
     Route::post('/attendances/clock-out', [AttendanceController::class, 'clock_out']);
-    Route::patch('/employees/{employee}', [AttendanceController::class, 'update']);
+    Route::patch('/employees/{employee}', [EmployeeController::class, 'update']);
     Route::get('/attendances/status', [AttendanceController::class, 'statusAttendance']);
     Route::get('/devisi/{id}', [DevisiController::class, 'getDevisi']);
     Route::post('/logout', [AuthController::class, 'logout']);
